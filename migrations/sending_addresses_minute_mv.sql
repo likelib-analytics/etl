7672948c57e36886110dfcd979f8f373bc2e19f1
Ivan Klimuk <ivanklimuk@Ivans-Evolution-MacBook-Pro.local>
2020-07-23 19:51:37 +0300

    Add sending addresses MV migrations

---
+++ migrations/sending_addresses_minute_mv.sql
@@ -0,0 +1,8 @@
+create materialized view if not exists sending_addresses_minute_mv
+engine = AggregatingMergeTree() order by (dt_minute)
+populate as
+select
+    toStartOfMinute(dt)     dt_minute
+    ,uniq(from)             sending_addresses
+from transactions
+group by dt_minute;
